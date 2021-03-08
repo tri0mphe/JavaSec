1b26f236d3460787b30b35c41550dc3f419aeb7d
l1825 <liukx@dtdream.com>
2021-03-08 10:29:30 +0800

    Test

---
+++ fastjson/src/main/java/com/triomphe/sec/MainTest.java
@@ -0,0 +1,11 @@
+package com.triomphe.sec;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+
+public class MainTest {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ fastjson/src/main/java/com/triomphe/util/Person.java
@@ -0,0 +1,31 @@
+package com.triomphe.util;
+
+public class Person {
+
+    private String userName;
+    private int age;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "userName='" + userName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
